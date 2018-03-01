14f55223061f37ac365a8efae9710ad4c279a522
Bryan Abate <bryan.abate@epfl.ch>
2018-03-01 20:32:46 +0100

    inout upd

--- inout.py
+++ inout.py
@@ -7,7 +7,7 @@ def read_file(file_name):
     df = pd.read_csv(file_name, sep=' ')
     df.columns = ['start_row', 'start_col', 'end_row', 'end_col', 'earliest_start', 'latest_end']
 
-    with open('a_example.in') as f:
+    with open(file_name) as f:
         first_line = re.sub(r'\n', '', f.readline())
 
     variables = dict(zip(['n_rows', 'n_cols', 'n_cars', 'n_rides', 'bonus', 'steps'], [int(st) for st in first_line.split(" ")]))
